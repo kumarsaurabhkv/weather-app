39b7a2c58f82f4c8e9d2b947dd3cf56427d068b0
Saurabh <you@example.com>
2024-07-06 14:20:51 +0530

    update

--- Form1.cs
+++ Form1.cs
@@ -18,6 +18,15 @@ namespace weather
         private void Getdata(string location)
         {
             var client = new RestClient($"https://wttr.in/{WebUtility.UrlEncode(location)}?format=%c+%t+%w+%h");
+            var request = new RestRequest();
+            request.AddParameter("method", "GET");
+
+            var response = client.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                string[] weatherParameter = Regex.Split(response.Content, " ");
+            }
         }
     }
 }
