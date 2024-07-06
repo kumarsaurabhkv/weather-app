b390502e88d4a5c28604faaee88e3f73cd192ce1
Madix <you@example.com>
2024-07-06 13:58:27 +0530

    update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApp1
+﻿namespace weather
 {
     partial class Form1
     {
--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp1
+namespace weather
+
 {
     public partial class Form1 : Form
     {
