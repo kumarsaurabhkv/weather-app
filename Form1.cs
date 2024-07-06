5959ebde08cf0e1e5dc2192d8b1606ae2bb8a688
Madix <you@example.com>
2024-07-06 13:58:27 +0530

    update

--- Form1.cs
+++ Form1.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Text.RegularExpressions;
+using RestSharp;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp1
+namespace weather
+
 {
     public partial class Form1 : Form
     {
